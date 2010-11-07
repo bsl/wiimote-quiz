7df7f2c0aaea07665c51417171e5683c719b6354
Brian Lewis <brian@lorf.org>
2010-11-07 17:14:16 -0500

    misc refactoring

--- controller.c
+++ controller.c
@@ -18,8 +18,8 @@
 /* - - - - - - - - - - - - - - - - - - - - */
 
 static void handle_button(struct button_event *b, rqueue_t hlcommandsq);
-static void flash_leds(struct button_event *b, rqueue_t hlcommandsq);
-static void rumble(struct button_event *b, rqueue_t hlcommandsq);
+static void flash_leds(int wiimote_num, rqueue_t hlcommandsq);
+static void rumble(int wiimote_num, rqueue_t hlcommandsq);
 
 /* - - - - - - - - - - - - - - - - - - - - */
 
@@ -55,65 +55,45 @@ controller_run(void *v)
 static void
 handle_button(struct button_event *b, rqueue_t hlcommandsq)
 {
+  const char *button_name;
+
   switch (b->button) {
-    case WIIMOTE_BUTTON_A:
-      print_info("button press event: id=%d, button=A", b->id);
-      flash_leds(b, hlcommandsq);
-      break;
-    case WIIMOTE_BUTTON_B:
-      print_info("button press event: id=%d, button=B", b->id);
-      break;
-    case WIIMOTE_BUTTON_UP:
-      print_info("button press event: id=%d, button=UP", b->id);
-      rumble(b, hlcommandsq);
-      break;
-    case WIIMOTE_BUTTON_DOWN:
-      print_info("button press event: id=%d, button=DOWN", b->id);
-      rumble(b, hlcommandsq);
-      break;
-    case WIIMOTE_BUTTON_LEFT:
-      print_info("button press event: id=%d, button=LEFT", b->id);
-      rumble(b, hlcommandsq);
-      break;
-    case WIIMOTE_BUTTON_RIGHT:
-      print_info("button press event: id=%d, button=RIGHT", b->id);
-      rumble(b, hlcommandsq);
-      break;
-    case WIIMOTE_BUTTON_MINUS:
-      print_info("button press event: id=%d, button=MINUS", b->id);
-      break;
-    case WIIMOTE_BUTTON_PLUS:
-      print_info("button press event: id=%d, button=PLUS", b->id);
-      break;
-    case WIIMOTE_BUTTON_ONE:
-      print_info("button press event: id=%d, button=ONE", b->id);
-      break;
-    case WIIMOTE_BUTTON_TWO:
-      print_info("button press event: id=%d, button=TWO", b->id);
-      break;
-    case WIIMOTE_BUTTON_HOME:
-      print_info("button press event: id=%d, button=HOME", b->id);
-      break;
-    default:
-      print_info("button press event: id=%d, button=%d", b->id, b->button);
-      break;
+    case WIIMOTE_BUTTON_A:     button_name = "A";         break;
+    case WIIMOTE_BUTTON_B:     button_name = "B";         break;
+    case WIIMOTE_BUTTON_UP:    button_name = "UP";        break;
+    case WIIMOTE_BUTTON_DOWN:  button_name = "DOWN";      break;
+    case WIIMOTE_BUTTON_LEFT:  button_name = "LEFT";      break;
+    case WIIMOTE_BUTTON_RIGHT: button_name = "RIGHT";     break;
+    case WIIMOTE_BUTTON_MINUS: button_name = "MINUS";     break;
+    case WIIMOTE_BUTTON_PLUS:  button_name = "PLUS";      break;
+    case WIIMOTE_BUTTON_ONE:   button_name = "ONE";       break;
+    case WIIMOTE_BUTTON_TWO:   button_name = "TWO";       break;
+    case WIIMOTE_BUTTON_HOME:  button_name = "HOME";      break;
+    default:                   button_name = "<unknown>"; break;
+  }
+
+  print_info("button pressed: wiimote %d, button %s", b->id, button_name);
+
+  switch (b->button) {
+    case WIIMOTE_BUTTON_A: flash_leds(b->id, hlcommandsq); break;
+    case WIIMOTE_BUTTON_B: rumble(b->id, hlcommandsq);     break;
   }
 }
 
-static void flash_leds(struct button_event *b, rqueue_t hlcommandsq)
+static void flash_leds(int wiimote_num, rqueue_t hlcommandsq)
 {
   struct highlevel_command *hlc;
 
-  hlc = highlevel_command_new(b->id, HIGHLEVEL_COMMAND_FLASH);
+  hlc = highlevel_command_new(wiimote_num, HIGHLEVEL_COMMAND_FLASH);
   hlc->parameters.flash.pattern_num = 0;
   rqueue_add(hlcommandsq, hlc);
 }
 
-static void rumble(struct button_event *b, rqueue_t hlcommandsq)
+static void rumble(int wiimote_num, rqueue_t hlcommandsq)
 {
   struct highlevel_command *hlc;
 
-  hlc = highlevel_command_new(b->id, HIGHLEVEL_COMMAND_RUMBLE);
+  hlc = highlevel_command_new(wiimote_num, HIGHLEVEL_COMMAND_RUMBLE);
   hlc->parameters.rumble.pattern_num = 0;
   rqueue_add(hlcommandsq, hlc);
 }
